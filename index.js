cd01fc68719be1667012962377df3f3c7179370c
kananidhavalkumar <kanani.dhavalkumar.542@ldce.ac.in>
2020-06-09 14:31:20 +0530

    move private info

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 
-mongoose.connect('mongodb+srv://DhavalKanani:Kds@9202@react-app-yu3ac.mongodb.net/test?retryWrites=true&w=majority',{useNewUrlParser:true,useUnifiedTopology: true})
+const {User} = require('./models/user')
+const config = require('./config/key')
+
+mongoose.connect(config.MongoURI,{useNewUrlParser:true,useUnifiedTopology: true})
 .then(() => {
   console.log("conncted")
 }).catch(err=>console.log("error"))
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+app.use(cookieParser())
+
+app.post('/api/users/register',(req,res) => {
+  const user = new User(req.body)
+  user.save((err,data) => {
+    if(err) return res.json({success:false,err})
+  })
+  return res.status(200).json({success:true})
+})
+
 
 app.listen(5000)
 app.get('/',(req,res) => {
